8f899b2922e4e91b204da859fd2913bcfa260a0e
Ryan Collingwood <ryancollingwood@gmail.com>
2019-08-17 21:18:29 +1000

    Update go_macosx.sh

--- go_macosx.sh
+++ go_macosx.sh
@@ -1,4 +1,4 @@
 #!
 sudo pip3 install virtualenv
 virtualenv .venv
-source .venv/bin/activate; pip3 install -r requirements.txt; pytest
+source .venv/bin/activate; pip3 install -r requirements.txt; pytest; python3 main.py
