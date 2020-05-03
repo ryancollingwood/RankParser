96cc854fc2a40a93b68ce8e28ebd8be4c205ca4c
Ryan Collingwood <ryancollingwood@gmail.com>
2020-05-03 12:53:35 +1000

    Added additional print outs to graphviz setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ ENV_VARIABLE_NAME = "GRAPHVIZ_DOT"
 WIN_GRAPHVIZ_URL = "https://graphviz.gitlab.io/_pages/Download/windows/graphviz-2.38.zip"
 DESTINATION_FILE = WIN_GRAPHVIZ_URL.split("/")[-1]
 INSTALL_TO_DIRECTORY = "".join(DESTINATION_FILE.split(".")[0:-1])
-DOT_PATH = f"{INSTALL_TO_DIRECTORY}/release/bin""
+DOT_PATH = f"{INSTALL_TO_DIRECTORY}/release/bin"
 
 
 def download_graphviz(url, destination_file):
@@ -21,12 +21,18 @@ def download_graphviz(url, destination_file):
 
 
 def extract_zip(file_name, destination_folder):
+    print(f"Extracting {file_name} to {destination_folder}")
     with ZipFile(file_name, 'r') as zip_ref:
         zip_ref.extractall(destination_folder)
 
+    os.remove(file_name)
+    print("Extraction complete")
+
 
 if ENV_VARIABLE_NAME not in os.environ:
-    print(f"Graphviz environment varialbe not set - will be downloaded to: {INSTALL_TO_DIRECTORY}")
+    print(f"Graphviz environment variable not set - will be downloaded to: {INSTALL_TO_DIRECTORY}")
     download_graphviz(WIN_GRAPHVIZ_URL, DESTINATION_FILE)
     extract_zip(DESTINATION_FILE, INSTALL_TO_DIRECTORY)
+
+    print("Setting Environment Variable")
     os.environ[ENV_VARIABLE_NAME] = DOT_PATH
