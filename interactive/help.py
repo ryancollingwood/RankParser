a9c9660c9f8f36e04de388b4679857c9dcb7b320
Ryan Collingwood <ryancollingwood@gmail.com>
2020-05-05 19:34:43 +1000

    added missing export_items help entry

--- interactive/help.py
+++ interactive/help.py
@@ -2,12 +2,16 @@ commands = {
     "+": ("Add an item: +<Item>", "+Ryan",),
     "-": ("Remove an item: - <Item>", "-Ryan"),
     "=": ("Solve and display results", "="),
-    "~": ("Suggest a pair for constraint specification.", "~"),
+    "~": ("Suggest a pair of items for constraint specification.", "~"),
     "?": ("Debug the input, displaying how it will be tokenised: ? <statement>", "? Breakfast before Dinner"),
     "history": ("Display the history of submitted statements", "history"),
     "undo": ("Remove the most recent submitted statement", "undo"),
     "load": ("Load a previous session: load <session_name>", "load programmer_riddle"),
     "copy": ("Copy the current session and change to it: copy <session>", "copy programmer_riddle_backup"),
+    "export_items": ("Export items to a text file. Can be used in import_items: "
+                     "export_items <filename>",
+                     "export_items exported_items"
+                     ),
     "import_items": ("Import items from a text file. "
                      "Each item must appear on it's own line in the text file: "
                      "import_items <filename>",
